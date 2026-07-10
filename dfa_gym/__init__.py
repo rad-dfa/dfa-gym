fcfdda6a2c5b889c0f6ffa4de52a515b1b6e05a7
Rudy Colato <rudycolato29@gmail.com>
2026-07-09 19:53:15 -0700

    add bisim env for dynamic alphabet

--- dfa_gym/__init__.py
+++ dfa_gym/__init__.py
@@ -1,5 +1,6 @@
 from dfa_gym.token_env import *
 from dfa_gym.dfa_bisim_env import *
+from dfa_gym.dfa_dyn_bisim_env import *
 from dfa_gym.dfa_wrapper import *
 from dfa_gym.env import *
 from dfa_gym.spaces import *
